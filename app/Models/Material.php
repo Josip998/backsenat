351a066a1b4ddc94c41fbd4eccbf61c477842a91
Josip <josip.bosnjak@fpmoz.sum.ba>
2023-10-20 03:08:01 +0200

    commit

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -9,18 +9,6 @@ class Material extends Model
 {
     protected $fillable = ['title', 'filename', 'point_id'];
 
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::deleting(function ($material) {
-            // Delete the associated file from storage
-            if (Storage::disk('public')->exists($material->filename)) {
-                Storage::disk('public')->delete($material->filename);
-            }
-        });
-    }
-
     public function point()
     {
         return $this->belongsTo(Point::class);
